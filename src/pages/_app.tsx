4d8bc76ec726ab76f2fa56b03c4574ebaea80fe4
jarangit <jaran.dch@gmail.com>
2024-02-14 13:13:35 +0700

    f

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,16 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <div className="w-full max-w-[1400px] mx-auto px-6">
+        <Component {...pageProps} />
+      </div>
+    </ClerkProvider>
+  )
 };
 
 export default api.withTRPC(MyApp);
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -10,18 +13,22 @@ export const postRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-
+  getAll: publicProcedure.query(({ ctx }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const data = ctx.db.post.findMany()
+    return data
+  }),
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
+      // return ctx.db.post.create({
+      //   data: {
+      //     name: input.name,
+      //   },
+      // });
     }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
